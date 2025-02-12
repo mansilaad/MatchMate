d58b1547cff6b8576bfb64a3019ce900224a450b
Mansi Laad <19mansilaad@gmail.com>
2025-02-12 23:05:29 +0530

    feat: folder structure

---
+++ MatchMate/Views/MatchListView.swift
@@ -0,0 +1,15 @@
+//
+//  MatchListView.swift
+//  MatchMate
+//
+//  Created by Mansi Laad on 12/02/25.
+//
+
+import SwiftUI
+
+struct MatchListView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
