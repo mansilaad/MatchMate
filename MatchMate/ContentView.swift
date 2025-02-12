907558f3a0c97e47ceb33816c63b9493f5ed377a
Mansi Laad <19mansilaad@gmail.com>
2025-02-12 22:00:32 +0530

    Initial Commit

---
+++ MatchMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchMate
+//
+//  Created by Mansi Laad on 12/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Mansi Laad on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
