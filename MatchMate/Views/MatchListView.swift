f387dd9130f9ef689a0376238c90f822795c17bb
Mansi Laad <19mansilaad@gmail.com>
2025-02-13 14:48:06 +0530

    feat: basic structuring

--- MatchMate/Views/MatchListView.swift
+++ MatchMate/Views/MatchListView.swift
@@ -2,13 +2,12 @@
 //  MatchListView.swift
 //  MatchMate
 //
-//  Created by Mansi Laad on 12/02/25.
+//  Created by Mansi Laad on 13/02/25.
 //
 
 import SwiftUI
 
 struct MatchListView: View {
-    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
