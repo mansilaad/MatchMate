6f5ec2489092b03583af83f3f82fae4a2e428db6
Mansi Laad <19mansilaad@gmail.com>
2025-02-14 08:34:41 +0530

    feat: final ui done

--- MatchMate/Views/MatchListView.swift
+++ MatchMate/Views/MatchListView.swift
@@ -11,44 +11,44 @@ struct MatchListView: View {
     @StateObject var viewModel = MatchViewModel()
     
     
-    
     var body: some View {
             ScrollView {
                 if viewModel.users.isEmpty {
                     ProgressView("Loading...")
                 } else {
-                    VStack(spacing: 20) {
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        Text("Profile Matches")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        
                         ForEach(viewModel.users) { user in
-                            //Text("✅ Rendering user: \(user.name.first) - ID: \(user.id)")
-                            
+                        
                             //MatchCardView(user: user, onAccept: {}, onDecline: {})
                         
                                 if !user.name.first.isEmpty && !user.name.last.isEmpty {
                                     VStack(alignment: .center, spacing: 12) {
-                                        Text("\(user.name.first) \(user.name.last), \(user.dob.age)")
-                                            .font(.headline)
-                                            .foregroundColor(.primary)
+                                        Text("\(user.name.first) \(user.name.last)")
+                                            .font(.title)
+                                            .foregroundColor(Color("MaxBlueGreen"))
+
 
                                         if !user.location.city.isEmpty && !user.location.state.isEmpty {
-                                            Text("\(user.location.city), \(user.location.state)")
+                                            Text("\(user.dob.age), \(user.location.city), \(user.location.state)")
                                                 .font(.subheadline)
                                                 .foregroundColor(.secondary)
                                         }
-
-                                        if let age = user.registered.age as Int? {
-                                            Text("\(age), \(user.location.city)").font(.subheadline)
-                                        }
-
+                                        
                                         HStack(spacing: 20) {
                                             Button(action: {}) {
-                                                Image(systemName: "xmark.circle.fill")
+                                                Image(systemName: "xmark.circle")
                                                     .resizable()
                                                     .frame(width: 50, height: 50)
-                                                    .foregroundColor(.red)
+                                                    .foregroundColor(Color("MaxBlueGreen"))
                                             }
                                             
                                             Button(action: {}) {
-                                                Image(systemName: "heart.circle.fill")
+                                                Image(systemName: "checkmark.circle")
                                                     .resizable()
                                                     .frame(width: 50, height: 50)
                                                     .foregroundColor(.green)
@@ -62,11 +62,10 @@ struct MatchListView: View {
                                 }
 
                             
-                                                    }
+                          }
                         }
                     .padding()
                 }
             }
-            .navigationTitle("MatchMate")
     }
 }
