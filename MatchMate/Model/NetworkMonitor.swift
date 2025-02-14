7bc35127830fafd0f6e574a2cc27a6302f5dbd60
Mansi Laad <19mansilaad@gmail.com>
2025-02-14 14:20:11 +0530

    feat: modified network monitor

--- MatchMate/Model/NetworkMonitor.swift
+++ MatchMate/Model/NetworkMonitor.swift
@@ -5,25 +5,20 @@
 //  Created by Mansi Laad on 13/02/25.
 //
 
+import Foundation
 import Network
-import Combine
 
 class NetworkMonitor: ObservableObject {
     static let shared = NetworkMonitor()
-    
     private let monitor = NWPathMonitor()
-    private let queue = DispatchQueue.global(qos: .background)
+    private let queue = DispatchQueue(label: "NetworkMonitor")
     
-    @Published var isConnected: Bool = true
+    @Published var isConnected = true
     
     private init() {
-        startMonitoring()
-    }
-    
-    private func startMonitoring() {
-        monitor.pathUpdateHandler = { [weak self] path in
+        monitor.pathUpdateHandler = { path in
             DispatchQueue.main.async {
-                self?.isConnected = path.status == .satisfied
+                self.isConnected = path.status == .satisfied
             }
         }
         monitor.start(queue: queue)
